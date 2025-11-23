87dd504b1ecef3cb1d02054f6a3d9595ffac9eac
itschool <itschool@tou.edu.kz>
2025-11-23 11:42:58 +0500

    скрытие текста

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // alert('retard all') 
 // выводит окошко на экран//
 
+// Только раскрытие текста
+// document.querySelector('.info__link').addEventListener('click', function () {
+//    this.remove();
+//    document.querySelector('.info__text').classList.add('info__text--active');
+// }); 
+
+let buttonText;  
+let buttonText2 = "Скрыть текст";
+
+// раскрыть_свернуть
 document.querySelector('.info__link').addEventListener('click', function () {
-    this.remove();
-    document.querySelector('.info__text').classList.add('info__text--active');
+    document.querySelector('.info__text').classList.toggle('info__text--active');
+    buttonText = this.textContent;
+    this.textContent = buttonText2;
+    buttonText2 = buttonText;
+
 });
+
+
+
+
+//document.querySelector('.info__link').addEventListener('click', function () {
+//    document.querySelector('.info__text').classList.toggle('info__text--active');
+//    if (this.textContent = 'Показать полностью') {
+//        this.textContent = "Скрыть текст"
+//    } else {
+//        this.textContent = "Показать полностью"
+//    }
